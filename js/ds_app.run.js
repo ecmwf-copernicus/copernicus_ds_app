7c18a722a110f31975a8e0fcbcff1fb22a363abd
narcis2005 <chirilov.narcis@yahoo.ro>
2024-08-05 11:47:22 +0300

    made js lazy load

--- js/ds_app.run.js
+++ js/ds_app.run.js
@@ -3,16 +3,30 @@
     attach: function attach(context, settings) {
       var $context = $(context);
       for (let i = 0; i < settings.ds_app_paragraphs.length; ++i) {
+        const appContainer = document.querySelector(`#ds-app-${ settings.ds_app_paragraphs[i] }`)
+        
         try {
-          window.cds_toolbox.runApp(
-            'ds-app-' + settings.ds_app_paragraphs[i],
-            'ds-app/' + settings.ds_app_paragraphs[i] + '/configuration.json',
-            {
-              monitorViewport: false,
-              standalone: false,
-              workflowBase: settings.cds_app_base_url
-            }
-          );
+          const observer = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                  window.cds_toolbox.runApp(
+                    'ds-app-' + settings.ds_app_paragraphs[i],
+                    'ds-app/' + settings.ds_app_paragraphs[i] + '/configuration.json',
+                    {
+                      monitorViewport: false,
+                      standalone: false,
+                      workflowBase: settings.cds_app_base_url
+                    }
+                  );
+                    observer.disconnect();
+                }
+            });
+        }, {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0
+        });
+        observer.observe(appContainer);     
         } catch (err) {
           console.error(err);
         }
